fb523aabf6ef8bc7851727886a2d262e55c74bbe
fer <ferp@protonmail.ch>
2020-01-29 20:33:20 +0100

    connected the server to a running dockerized mongodb on port 27018

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
 require("dotenv").config();
 
 const express = require("express")
+const mongoose = require("mongoose")
 
 //instantiate server
 const server = express();
 console.log("Server running and listening on port: " + process.env.PORT);
+
+//connect to DB
+mongoose
+  .connect(process.env.DBURL, {useNewUrlParser: true})
+  .then( db => 
+    console.log(
+      `Connected to Mongo! Database name: ${db.connections[0].name}`
+    )
+  )
+
+//autoDisconnect from mongo on SIGINT
+process.on("SIGINT", () => {
+  mongoose.connection.close(() => {
+    console.log(
+      "<Mongoose default connection disconnected through app termination"
+    );
+    process.exit(0);
+  })
+})
 
 server.listen(3010)
