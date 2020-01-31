45d441797bc5479a2737031c4a0075adb3f6340d
fer <ferp@protonmail.ch>
2020-01-31 13:27:35 +0100

    added Beer incomplete model

---
+++ api/models/Beer.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const beerSchema = Schema({
+  id: Number,
+  name: String,
+  tagline: String,
+  first_brewed: String,
+  description: String,
+  image_url: String,
+  abv: Number,
+  ibu: Number,
+  target_fg: Number,
+  target_og: Number,
+  ebc: Number,
+  srm: Number,
+  ph: Number,
+  attenuation_level: Number,
+  volume: {
+    value: Number,
+    unit: String
+  },
+  boil_volume: {
+    value: Number,
+    unit: String
+  },
+  //add method
+  //add ingredients
+  food_pairing: [ String ],
+  brewers_tips: String,
+  contributed_by: String
+    
+}, {
+  timestamps: {
+    createdAt: "created_at",
+    updatedAt: "updated_at"
+  }
+})
