043b584eb9ec1ed8bfc28e3c03a686442340cce8
fer <ferp@protonmail.ch>
2020-02-07 12:51:50 +0100

    added route for creating new beers, also minor spelling corrections

--- api/controllers/beers.js
+++ api/controllers/beers.js
@@ -5,19 +5,30 @@ const Beer = require("../models/Beer.js")
 module.exports = {
   async getBeers(){
     try {
-    return Beer.find()
+    return await Beer.find()
     } catch (err) {
-      console.error("[Controler] error retrieving beers from the DB", err)
+      console.error("[Controller] Error retrieving beers from the DB", err)
     }
   },
   
   async getBeerById(beerId){
     try {
-    return Beer.find({
+    return await Beer.find({
       id: beerId 
     })
     } catch (err) {
-      console.error("[Controler] error retrieving beer with id " + beerId  + "  from the DB", err)
+      console.error("[Controller] Error retrieving beer with id " + beerId  + "  from the DB", err)
     }
+  },
+
+  async createBeer(body){
+    const NewBeer = new Beer({
+      ...body
+    })
+    const createdBeer = await NewBeer.save()
+      .catch( err =>{
+        console.error("[Controller] Error saving new beer", err)
+      })
+    return createdBeer 
   }
 }
